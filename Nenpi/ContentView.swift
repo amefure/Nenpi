1c5a51e2f760e1098da0757eed09ead123624d62
ame <shachi10969@gmail.com>
2022-07-02 08:16:46 +0900

    Initial Commit

---
+++ Nenpi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nenpi
+//
+//  Created by t&a on 2022/07/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nenpi/NenpiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NenpiApp.swift
+//  Nenpi
+//
+//  Created by t&a on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct NenpiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
