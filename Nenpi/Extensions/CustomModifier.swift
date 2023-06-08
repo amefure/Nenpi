3926a9a4ff0524a8844fb4e7f9c8302e0b84ca2d
ame <shachi10969@gmail.com>
2023-06-08 10:16:48 +0900

    Add DeviceSizeViewModel

---
+++ Nenpi/Extensions/CustomModifier.swift
@@ -0,0 +1,22 @@
+//
+//  CustomModifier.swift
+//  Nenpi
+//
+//  Created by t&a on 2023/06/07.
+//
+
+import SwiftUI
+
+
+extension Image {
+
+    
+    func ex_ResizableTopIconModifier() -> some View {
+        self
+            .resizable(resizingMode: .stretch)
+            .frame(width: DeviceSizeViewModel().isSESize ? 100.0 : 150.0, height: DeviceSizeViewModel().isSESize ? 100.0 : 150.0)
+            .padding(.bottom,(DeviceSizeViewModel().isSESize ? 30 : 145.0))
+            .foregroundColor(Color.white)
+    }
+}
+
---
+++ Nenpi/ViewModels/DeviceSizeViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  DeviceSizeViewModel.swift
+//  Nenpi
+//
+//  Created by t&a on 2023/06/07.
+//
+
+import UIKit
+
+class DeviceSizeViewModel {
+    
+    public let deviceWidth = UIScreen.main.bounds.width
+    public let deviceHeight = UIScreen.main.bounds.height
+    // 端末設定のカレンダー
+    public let calendar = Calendar.current
+    
+    public var isSESize:Bool {
+        if deviceWidth < 400{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    public var isiPadSize:Bool {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    public var flexWidth:Double {
+        if self.isiPadSize {
+            return self.deviceWidth/1.5
+        }else{
+            return self.deviceWidth
+        }
+    }
+}
